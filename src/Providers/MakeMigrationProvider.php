ee0cef6d46ab1436ac3bea816769d4982630b9a0
Gabriel Nwogu <nwogugabriel@gmail.com>
2019-07-05 12:46:22 +0100

    add make migration provider

---
+++ src/Providers/MakeMigrationProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Nwogu\SmoothMigration\Providers;
+
+use Illuminate\Database\MigrationServiceProvider;
+use Nwogu\SmoothMigration\Console\MigrateMakeCommand;
+
+class MakeMigrationProvider extends MigrationServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        parent::register();
+
+        $this->registerMigrateMakeCommand();
+    }
+
+     /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerMigrateMakeCommand()
+    {
+        $this->app->singleton('command.migrate.make', function ($app) {
+            // Once we have the migration creator registered, we will create the command
+            // and inject the creator. The creator is responsible for the actual file
+            // creation of the migrations, and may be extended by these developers.
+            $creator = $app['migration.creator'];
+
+            $composer = $app['composer'];
+
+            return new MigrateMakeCommand($creator, $composer);
+        });
+    }
+
+}
